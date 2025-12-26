fa6fefe8862aff636d9a7e05e28159b30523a645
acj81 <a_c_jones@tutanota.com>
2025-12-26 22:50:34 +0000

    added shared NN model class

---
+++ main.py
@@ -0,0 +1,102 @@
+import torch
+import numpy as np
+import pandas as pd
+import models
+import datasets
+
+from torch.utils.data import DataLoader
+from torch import nn
+
+if __name__ == "__main__":
+        
+    # test perceptron 
+    model = models.Perceptron(
+        inp_size = 28
+    )
+
+    # test forward prop:
+    features = torch.randn(5, 28)
+    labels = torch.randn(5, 1)
+    pred = model(features)
+
+    # --- APPLICATION ---
+    # -- CHANGE MODEL AS WE TEST:
+    model = models.Perceptron(
+        inp_size = 29
+    )
+    model.to("cpu")
+
+    # get DataFrame from .csv, split into train and test datasets:
+    data = pd.read_csv("creditcard.csv")
+    partition_idx = int(len(data) * 0.8)
+    # get all 28 feature columns without having to manually specify
+    feature_cols = [x for x in data.columns if (x[0] == "V" or x == "Amount")]
+    features = data[feature_cols]
+    labels = data[["Class"]]
+    # create individual datasets - use partition idx to split features and labels into test and train datasets:
+    train_data = datasets.CSVDataset(features.iloc[:partition_idx], labels.iloc[:partition_idx])
+    test_data = datasets.CSVDataset(features.iloc[partition_idx:], labels.iloc[partition_idx:])
+    # pass to DataLoaders:
+    train_loader = DataLoader(
+        train_data, 
+        batch_size=64,
+        shuffle=True
+    )
+    test_loader = DataLoader(
+        test_data, 
+        batch_size=64,
+        shuffle=True
+    )
+    
+    # --- PERCEPTRON ---
+    print(f"--- PERCEPTRON ---")
+
+    # test on data:
+    loss = model.test_loop(
+        data_loader = test_loader,
+        loss_fn = nn.MSELoss()
+    )
+    print(f"start loss: {loss}")
+    
+    # train on data:
+    model.train_loop(
+        data_loader = train_loader,
+        num_epochs = 10,
+        loss_fn = nn.MSELoss(),
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+    )
+
+    # test on data:
+    loss = model.test_loop(
+        data_loader = test_loader,
+        loss_fn = nn.MSELoss()
+    )
+    print(f"end loss: {loss}")
+
+    # --- 2-layer NN --- :
+    print(f" --- 2-layer NN --- ")
+    model = models.NeuralNet(
+        dimensions = [[28, 14],[14, 1]]
+    )
+
+    # test on data:
+    loss = model.test_loop(
+        data_loader = test_loader,
+        loss_fn = nn.MSELoss()
+    )
+    print(f"start loss: {loss}")
+    
+    # train on data:
+    model.train_loop(
+        data_loader = train_loader,
+        num_epochs = 10,
+        loss_fn = nn.MSELoss(),
+        optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+    )
+
+    # test on data:
+    loss = model.test_loop(
+        data_loader = test_loader,
+        loss_fn = nn.MSELoss()
+    )
+    print(f"end loss: {loss}")
--- models.py
+++ models.py
@@ -2,146 +2,71 @@ import torch
 import numpy as np
 import pandas as pd
 from torch import nn
-from torch.utils.data import DataLoader
-from copy import deepcopy
 
 import datasets
 
-# 3 models - perceptron, basic Neural Net, Deep Neural Net
-class Perceptron:
-    def __init__(self, inp_size, loss_fn, lr):
-        # set weights and biases - requires_grad so we can do backprop later:
-        self.weights = torch.randn(inp_size, 1, requires_grad=True)
-        self.bias = torch.randn(1, requires_grad=True)
-        self.loss_fn = loss_fn
-        self.lr = lr
+class NeuralNet(nn.Module):
+    def __init__(self, dimensions):
+        super().__init__()
+        # declare initial layers:
+        self.layers = nn.Sequential()
+        # add layers based on dimensions given:
+        for in_size, out_size in dimensions:
+            # add linear layer w/ dimensions passed:
+            self.layers.append(nn.Linear(in_size, out_size)) 
+            # add ReLU layer after for stack:
+            self.layers.append(nn.ReLU())
 
     def forward(self, features):
         # calculate:
-        pred_labels = features @ self.weights + self.bias
+        pred_labels = self.layers(features)
         return pred_labels
-    
-    def train_on_batch(self, features, labels):
-        # forward to get predictions:
-        pred_labels = self.forward(features)
-        # get loss using predicted and actual labels:
-        loss = self.loss_fn(pred_labels, labels)
-        # apply to each of them:
-        loss.backward()
-        w_grad, b_grad = self.weights.grad, self.bias.grad
-        with torch.no_grad():
-            self.weights += w_grad * self.lr
-            self.bias += b_grad * self.lr
-        # clear gradient so doesn't build up over time:
-        self.weights.grad.zero_()
-        self.bias.grad.zero_()
 
-    def test_on_batch(self, features, labels):
-        # no grad so doesn't interfere with gradients:
-        with torch.no_grad():
-            # forward to get predictions:
-            pred_labels = self.forward(features)
-            # get average loss between predicted and actual labels:
-            loss = self.loss_fn(pred_labels, labels)
-            avg_loss = loss.item() / len(features)
-        return avg_loss 
-        
+    def train_loop(
+        self,
+        data_loader,
+        num_epochs, 
+        optimizer,
+        loss_fn,
+    ):
+        # iterate for epochs:
+        for epoch in range(num_epochs):
+            print(f"training epoch: {epoch}")
+            # iterate for each batch:
+            for features, labels in data_loader:
+                # get predictions for batch:
+                pred_labels = self(features)
+                # get loss and backpropagate for gradients:
+                loss = loss_fn(pred_labels, labels)
+                loss.backward()
+                # step optimizer to apply gradients and then reset those gradients w/ zero_grad:
+                optimizer.step()
+                optimizer.zero_grad()
 
-    def test_on_loader(self, loader):
-        # average loss we return @ the end:
+    def test_loop(
+            self, 
+            data_loader, 
+            loss_fn
+    ):
+        self.eval()
+        # average loss value we return:
         avg_loss = 0
-        for batch_num, (batch_features, batch_labels) in enumerate(loader):
-            #print(f"testing batch: {batch_num}")
-            # get loss for this batch:
-            batch_loss = self.test_on_batch(batch_features, batch_labels)
-            avg_loss += batch_loss
-        # divide to get average:
-        avg_loss /=  len(loader)
+        total_examples = 0
+        # don't need to calculate gradients so more efficient here:
+        with torch.no_grad():
+                # calculate loss for each batch:
+                for features, labels in data_loader:
+                    # get predictions, get loss for those predictions
+                    pred_labels = self(features)
+                    loss = loss_fn(pred_labels, labels)
+                    avg_loss += loss
+                    # increment total_examples:
+                    total_examples += len(features)
+        # divide to get average loss @ end:
+        avg_loss /= total_examples
         return avg_loss
-
-    def train_on_loader(self, loader, num_epochs):
-        for i in range(num_epochs):
-            print(f"training epoch: {i}")
-            for batch_num, (batch_features, batch_labels) in enumerate(loader):
-                #print(f"training batch: {batch_num}")
-                # train on batch:
-                self.train_on_batch(batch_features, batch_labels)
-
-# -- testing ---
-
-if __name__ == "__main__":
-        
-    # test perceptron 
-    model = Perceptron(
-        inp_size = 5,
-        loss_fn = nn.MSELoss(),
-        lr = 0.01    
-    )
-
-    # test forward prop:
-    features = torch.randn(5, 5)
-    labels = torch.randn(5, 1)
-    pred = model.forward(features)
-
-    # test backpropagation is updated model parameters:
-    original_weights, original_bias = deepcopy(model.weights), deepcopy(model.bias)
-    model.train_on_batch(features, labels)
-    new_weights, new_bias = model.weights, model.bias
-    # see if backprop updated weights:
-    if (original_weights == new_weights).all():
-        raise Exception("weights weren't updated")
-    # see if backprop updated bias:
-    if (original_bias == new_bias).all():
-        raise Exception("bias wasn't updated")
-    
-    # make sure test function works:
-    loss_value = model.test_on_batch(features, labels)
-    #print(loss_value)
-
-    # --- APPLICATION ---
-    # -- CHANGE MODEL AS WE TEST:
-    model = Perceptron(
-        inp_size = 29,
-        loss_fn = nn.MSELoss(),
-        lr = 0.01    
-    )
-
-    # get DataFrame from .csv, split into train and test datasets:
-    data = pd.read_csv("creditcard.csv")
-    partition_idx = int(len(data) * 0.8)
-    # get all 28 feature columns without having to manually specify
-    feature_cols = [x for x in data.columns if (x[0] == "V" or x == "Amount")]
-    features = data[feature_cols]
-    labels = data[["Class"]]
-    # create individual datasets - use partition idx to split features and labels into test and train datasets:
-    train_data = datasets.CSVDataset(features.iloc[:partition_idx], labels.iloc[:partition_idx])
-    test_data = datasets.CSVDataset(features.iloc[partition_idx:], labels.iloc[partition_idx:])
-    # pass to DataLoaders:
-    train_loader = DataLoader(
-        train_data, 
-        batch_size=64,
-        shuffle=True
-    )
-    test_loader = DataLoader(
-        test_data, 
-        batch_size=64,
-        shuffle=True
-    )
-    
-    # test on data:
-    loss = model.test_on_loader(
-        loader = test_loader
-    )
-    print(f"start loss: {loss}")
-    
-    # train on data:
-    model.train_on_loader(
-        loader = train_loader,
-        num_epochs = 10
-    )
-
-    # test on data:
-    loss = model.test_on_loader(
-        loader = test_loader
-    )
-    print(f"end loss: {loss}")
+                    
+class Perceptron(NeuralNet):
+    def __init__(self, inp_size):
+        ''' just a wrapper for NeuralNet so it can use inp_size instead of tuple of dimensions '''
+        NeuralNet.__init__(self, [[inp_size, 1]])
